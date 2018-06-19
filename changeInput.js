69f9b34d6af67671cc5e184a2b4cbdce4ad1b3f6
lucasnoveline <lucasnoveline@gmail.com>
2018-06-19 19:39:24 -0300

    Update changeInput.js

--- changeInput.js
+++ changeInput.js
@@ -24,18 +24,28 @@ function changeInput () {
 			var dia = Number(val) - 16;
 			var data = '07.' + dia;
 		}
-		output.innerHTML = data;
-	}
+		// Modificando tabela de grupos
+		var tables = document.getElementsByClassName("tables");
+		var i;
 
-	// Modificando tabela de grupos
-	var tables = document.getElementsByClassName("tables");
-	var i;
+		// Para cada tabela de grupos
+		for (i = 0; i < tables.length; i++) { 
+		    var group = tables[i].id;
+		    var frame = "<iframe width='300' height='200' frameborder='0' scrolling='no' src='https://lucasnoveline.github.io/tables_test2?file_path=" + group + "/" + data + ".json' align='middle'></iframe>";
+		    console.log(frame);
+		    tables[i].innerHTML = frame;
+		}
+		}
 
-	// Para cada tabela de grupos
-	for (i = 0; i < tables.length; i++) { 
-	    var group = tables[i].id;
-	    var frame = "<iframe width='300' height='200' frameborder='0' scrolling='no' src='https://lucasnoveline.github.io/tables_test2?file_path=" + group + "/" + data + ".json' align='middle'></iframe>";
-	    console.log(frame);
-	    tables[i].innerHTML = frame;
-}
+		// Modificando tabela de grupos
+		var tables = document.getElementsByClassName("tables");
+		var i;
+
+		// Para cada tabela de grupos
+		for (i = 0; i < tables.length; i++) { 
+		    var group = tables[i].id;
+		    var frame = "<iframe width='300' height='200' frameborder='0' scrolling='no' src='https://lucasnoveline.github.io/tables_test2?file_path=" + group + "/" + data + ".json' align='middle'></iframe>";
+		    console.log(frame);
+		    tables[i].innerHTML = frame;
+		}
 }
