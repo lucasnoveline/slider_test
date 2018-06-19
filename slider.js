706166cfd7ce6481362eacca8161e4e6d2272ad3
lucasnoveline <lucasnoveline@gmail.com>
2018-06-19 17:35:39 -0300

    Update slider.js

--- slider.js
+++ slider.js
@@ -10,9 +10,18 @@ function getValue () {
 		var dia = val - 16;
 		var data = dia + ' de Julho';
 	}
-	output.innerHTML = slider.value;
+	output.innerHTML = data;
 
 	slider.oninput = function() {
-	  output.innerHTML = this.value;
+		var val = this.value;
+		if (val < 17) {
+			var dia = val + 14;
+			var data = dia + ' de Junho';
+		}
+		else {
+			var dia = val - 16;
+			var data = dia + ' de Julho';
+		}
+		output.innerHTML = data;
 	}
 }
