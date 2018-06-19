b6e49bba59f4b909a65adc9649eab4f6f1825668
lucasnoveline <lucasnoveline@gmail.com>
2018-06-19 17:39:16 -0300

    Update getValue.js

--- getValue.js
+++ getValue.js
@@ -1,9 +1,27 @@
 function getValue () {
 	var slider = document.getElementById("myRange");
 	var output = document.getElementById("demo");
-	output.innerHTML = slider.value;
+	var val = slider.value;
+	if (val < 17) {
+		var dia = val + 14;
+		var data = dia + ' de Junho';
+	}
+	else {
+		var dia = val - 16;
+		var data = dia + ' de Julho';
+	}
+	output.innerHTML = data;
 
 	slider.oninput = function() {
-	  output.innerHTML = this.value;
+		var val = this.value;
+		if (val < 17) {
+			var dia = val + 14;
+			var data = dia + ' de Junho';
+		}
+		else {
+			var dia = val - 16;
+			var data = dia + ' de Julho';
+		}
+		output.innerHTML = data;
 	}
 }
