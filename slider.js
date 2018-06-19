b393539b4e5a6383477f6c0d0ed8fe0fb027ac7e
lucasnoveline <lucasnoveline@gmail.com>
2018-06-19 17:34:10 -0300

    Update slider.js

--- slider.js
+++ slider.js
@@ -1,6 +1,15 @@
 function getValue () {
 	var slider = document.getElementById("myRange");
 	var output = document.getElementById("demo");
+	var val = slider.value;
+	if (val < 17) {
+		var dia = val + 14;
+		var data = dia + ' de Junho';
+	}
+	else {
+		var dia = val - 16;
+		var data = dia + ' de Julho';
+	}
 	output.innerHTML = slider.value;
 
 	slider.oninput = function() {
